dc4143973d320f9695f8bb6a4dd7bcde5020c2e5
Carlos <sscarlos3435@gmail.com>
2023-09-19 16:52:40 +0100

    TODO commit

--- 1. Fundamentals/Variables/1.3 Cafe Receipt/src/CafeReceipt.java
+++ 1. Fundamentals/Variables/1.3 Cafe Receipt/src/CafeReceipt.java
@@ -4,12 +4,16 @@ public class CafeReceipt
    {
       //Information to be printed.
       /* Declare and initialize the variables used below. */
-
+      String customerName = "Hisao";
+      int cupsOfCoffee = 1;
+      double costOfCoffee = 99.9;
+      boolean tip = true;
       //Print that information in the console.
       System.out.println("Dear " + customerName + ", thank you for supporting our business!");
       System.out.println("Cups of coffee bought: " + cupsOfCoffee + ".");
       System.out.println("Cost of 1 cup of coffee: $" + costOfCoffee + ".");
       System.out.println("Your total is: $99.9.");
       System.out.print("Tip is included? " + tip + ".");
+      //TODO run the project
    }
 }
