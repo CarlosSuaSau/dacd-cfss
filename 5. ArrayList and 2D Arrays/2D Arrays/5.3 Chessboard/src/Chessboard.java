6a525bab66edd4b2f37e7ed29acc92d1d6d50da5
Carlos <sscarlos3435@gmail.com>
2023-09-28 18:36:52 +0100

    Curso Terminado

--- 5. ArrayList and 2D Arrays/2D Arrays/5.3 Chessboard/src/Chessboard.java
+++ 5. ArrayList and 2D Arrays/2D Arrays/5.3 Chessboard/src/Chessboard.java
@@ -4,6 +4,19 @@ public class Chessboard
    {
       char[][] chessboard = new char[8][8];
 
-      /* Create and print a chessboard. */
+      for (int i = 0; i < 8; i++) {
+         if (i % 2 == 0) {
+            chessboard[i] = new char[]{'\u25A1', '\u25A0', '\u25A1', '\u25A0', '\u25A1', '\u25A0', '\u25A1', '\u25A0'};
+         } else {
+            chessboard[i] = new char[]{'\u25A0', '\u25A1', '\u25A0', '\u25A1', '\u25A0', '\u25A1', '\u25A0', '\u25A1'};
+         }
+      }
+
+      for (int x = 0; x < 8; x++) {
+         for (int y = 0; y < 8; y++) {
+            System.out.print(chessboard[x][y]);
+         }
+         System.out.println();
+      }
    }
 }
